7fcd726d41796b569f9b0420f9f4287c7b13c078
andradeju <julliana.andrade08@gmail.com>
2023-04-19 18:21:53 -0300

    Add CRUD alunos

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ app.use(morgan("dev"));
 const { connection, authenticate } = require ("./database/database");
 authenticate(connection);
 
+//Definição de Rotas
+const rotasAlunos = require("./routes/alunos");
+
 //TODO: Chamar banco de dados { authenticate, connection }
 
 //TODO: Chamar rotas (require) e (app.use())
+app.use(rotasAlunos);
 
 app.listen(3000, () => {
-    //connection.sync();
+    connection.sync();
     console.log("Servidor rodando em http://localhost:3000");
 });
